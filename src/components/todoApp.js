85fa5c6cf46d44d23e5cc1d47e0c6c448dd1a314
ErickDC <erickdiazc717@gmail.com>
2023-02-05 13:49:41 -0500

    background-color changed

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -5,7 +5,7 @@ import './todoApp.css'
 export default function TodoApp() {
     const [title, setTitle] = useState("");
     const [todos, setTodos] = useState([]);
-    const [editItem, setEditItem] = useState(null);
+    /* const [editItem, setEditItem] = useState(null); */
 
     function handleInputChange(e) {
         setTitle(e.target.value);
