60bba080938090cd9aefc0b5aeb59294bfd47515
ErickDC <erickdiazc717@gmail.com>
2023-02-05 13:39:59 -0500

    todo and todoApp component changed

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,54 +1,81 @@
 import { useState } from "react"
+import Todo from "./todo";
 
 export default function TodoApp() {
     const [title, setTitle] = useState("");
     const [todos, setTodos] = useState([]);
+    const [editItem, setEditItem] = useState(null);
 
-    function handleChange(event) {
-        const value = event.target.value;
-        setTitle(value);
+    function handleInputChange(e) {
+        setTitle(e.target.value);
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-
         const newTodo = {
-            id: crypto.randomUUID(),
+            id: Date.now(),
             title: title,
-            completed: false
+            completed: false,
         };
 
-        const temp = [...todos];
-        temp.unshift(newTodo);
+        const oldTodos = [...todos];
+        oldTodos.unshift(newTodo);
 
-        setTodos(temp);
+        setTodos(oldTodos);
+        setTitle("");
+    }
+
+    function handleDelete(id) {
+        const tempTodos = todos.filter((item) => item.id !== id);
+
+        setTodos([...tempTodos]);
+    }
+
+    function handleUpdate(id, value) {
+        const temp = [...todos];
+        const item = temp.find((item) => item.id === id);
+        item.title = value;
+        setTodos([...temp]);
+    }
+
+    function handleCheckboxChange(id, status) {
+        const temp = [...todos];
+        const item = temp.find((item) => item.id === id);
+        item.completed = status;
+
+        console.log("Holis");
+        setTodos([...temp]);
     }
 
     return (
         <div className="todoContainer">
-            <form className="todoCreateForm" onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="todoCreateForm">
                 <input
-                    type="text"
+                    onChange={handleInputChange}
                     value={title}
                     className="todoInput"
-                    onChange={handleChange}
                 />
                 <input
-                    type="submit"
                     value="Create todo"
+                    type={"submit"}
                     className="buttonCreate"
-                    onClick={handleSubmit}
                 />
             </form>
 
             <div className="todosContainer">
                 {
-                    todos.map(item => (
-                        <div key={item.id}>{item.title}</div>
+                    todos.map((item) => (
+                        <Todo
+                            key={item.id}
+                            item={item}
+                            onUpdate={handleUpdate}
+                            onDelete={handleDelete}
+                            onComplete={handleCheckboxChange}
+                        />
                     ))
                 }
             </div>
         </div>
-    )
+    );
 }
 
